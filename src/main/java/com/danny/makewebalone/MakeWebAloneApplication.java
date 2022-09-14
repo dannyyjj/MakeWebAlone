3a9cf9054eb77cc5aea5342d559778dcd0406a3c
danny <danny-j@naver.com>
2022-09-14 15:40:37 +0900

    Initial commit

---
+++ src/main/java/com/danny/makewebalone/MakeWebAloneApplication.java
@@ -0,0 +1,13 @@
+package com.danny.makewebalone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeWebAloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakeWebAloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/danny/makewebalone/MakeWebAloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danny.makewebalone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeWebAloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
