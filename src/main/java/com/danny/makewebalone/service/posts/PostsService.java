b7b267c11c6a0613df1f67d5fb0d5fd39ed3f524
정대현 <dannyyjj123@gmail.com>
2022-09-29 20:31:00 +0900

    0929 게시글 수정, 삭제 추가

--- src/main/java/com/danny/makewebalone/service/posts/PostsService.java
+++ src/main/java/com/danny/makewebalone/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.danny.makewebalone.service.posts;
 
 import com.danny.makewebalone.web.domain.posts.Posts;
 import com.danny.makewebalone.web.domain.posts.PostsRepository;
+import com.danny.makewebalone.web.dto.PostsListResponseDto;
 import com.danny.makewebalone.web.dto.PostsResponseDto;
 import com.danny.makewebalone.web.dto.PostsSaveRequestDto;
 import com.danny.makewebalone.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +39,20 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
