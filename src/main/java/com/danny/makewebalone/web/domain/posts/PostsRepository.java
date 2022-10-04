17a51bf1da2fb9d8a1123aabd3b3e18116a020c6
danny <dannyyjj123@gmail.com>
2022-10-04 19:24:12 +0900

    1004 구글, 네이버 이상 무

--- src/main/java/com/danny/makewebalone/web/domain/posts/PostsRepository.java
+++ src/main/java/com/danny/makewebalone/web/domain/posts/PostsRepository.java
@@ -9,6 +9,5 @@ public interface PostsRepository extends JpaRepository<Posts, Long> {
 
     @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
     List<Posts> findAllDesc();
-    // int sql_Post(Posts posts);
 
 }
