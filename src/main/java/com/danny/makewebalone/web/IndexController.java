5cf1341ca63373c690dfdcd496d39fe317ca7e60
danny <dannyyjj123@gmail.com>
2022-10-05 21:28:12 +0900

    1005 Mysql진행중

--- src/main/java/com/danny/makewebalone/web/IndexController.java
+++ src/main/java/com/danny/makewebalone/web/IndexController.java
@@ -24,7 +24,7 @@ public class IndexController {
         model.addAttribute("posts", postsService.findAllDesc());
         // SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user!=null){
-            model.addAttribute("userName", user.getName());
+            model.addAttribute("userName2", user.getName());
         }
 
         return "index";
