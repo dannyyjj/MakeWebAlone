6d11dfe2cd0039af2b6de20fdd9ae4bd4edb76f6
Danny <dannyyjj123@gmail.com>
2023-01-11 18:16:26 +0900

    0111) Test Test

--- src/test/java/com/danny/makewebalone/web/PostsApiControllerTest.java
+++ src/test/java/com/danny/makewebalone/web/PostsApiControllerTest.java
@@ -10,6 +10,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -36,13 +37,10 @@ public class  PostsApiControllerTest {
 
     @LocalServerPort
     private int port;
-
     @Autowired
     private TestRestTemplate restTemplate;
-
     @Autowired
     private PostsRepository postsRepository;
-
     @Autowired
     private WebApplicationContext context;
 
